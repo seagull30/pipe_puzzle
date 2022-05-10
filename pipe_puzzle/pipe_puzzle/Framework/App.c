64274d201f25e7e951163cdf5733b0df3ede55ca
seagull30 <minsang22@gmail.com>
2022-05-10 17:09:03 +0900

    fps 설정

--- pipe_puzzle/pipe_puzzle/Framework/App.c
+++ pipe_puzzle/pipe_puzzle/Framework/App.c
@@ -1,6 +1,7 @@
 #include "App.h"
 #include "common.h"
 #include "Renderer.h"
+#include "timer.h"
 
 bool App_Init()
 {
@@ -16,10 +17,27 @@ void processInput()
 
 }
 
+float count = 0;
 void undate()
 {
-	Renderer_DrawText("hello game", sizeof("hello game"));
+	// 비례식
+	// 프레임 : 시간
+	// 1 : deltaTime = fps : 1
+	// 1 = deltaTime * fps 
+	// fps = 1 /deltaTime
+	count += DELTA_TIME;
+	if (count > 0.5 && count < 1)
+	{
+		char str[128] = "";
+		sprintf_s(str, sizeof(str), "현재 FPS : %d", (int32)(1 / Timer_GetDeltaTime()));
+		Renderer_DrawText(str, strlen(str));
+	}
+	else if (count > 1)
+		count = 0;
+
+	
 }
+
 
 void render()
 {
@@ -28,19 +46,28 @@ void render()
 
 void cleanup()
 {
-
+	Renderer_Cleanup();
 }
 
 int32 App_Run()
 {
 	atexit(cleanup);
 
+	Timer_Init(60);
+
+	const int32 FIXED_FPS = 60;
+	const float FIXED_TIME = 1.0f / FIXED_FPS;
+	
 	// game loop : 게임을 동작시키는 루프. 프레임(Frame)이라고 한다.
 	while (true)
 	{
+		if (Timer_Update()) //deltatime : 프레임 간의 시간. 이전 프레임으로부터 현재 프레임을 시작할 때까지 걸린 시간
+		{
 		processInput(); // 입력 처리
 		undate(); // 게임 업데이트
 		render(); //게임 출력
+
+		} 
 	}
 	return 0;
 }
